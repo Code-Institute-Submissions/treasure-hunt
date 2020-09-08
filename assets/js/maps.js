353361c34eb126fbeeb0bea54df74047a4e89975
stefbez <stefbez@hotmail.com>
2020-09-08 13:04:29 +0000

    add markers to global variable to use the same markers for easy and hard maps

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,9 +1,10 @@
+var bermudaLatLng = { lat: 32.313327, lng: -64.747878 };
+var jamaicaLatLng = { lat: 18.1198291, lng: -77.8365849 };
+var barbadosLatLng = { lat: 13.1884729, lng: -59.6753414 };
+var antiguaLatLng = { lat: 17.0858213, lng: -61.8596279 };
+var cubaLatLng = { lat: 21.4653347, lng: -84.0583324 };
+
 function initMap(easyMap) {
-  var bermudaLatLng = { lat: 32.313327, lng: -64.747878 };
-  var jamaicaLatLng = {lat: 18.1198291, lng: -77.8365849};
-  var barbadosLatLng = {lat: 13.1884729, lng: -59.6753414};
-  var antiguaLatLng = {lat: 17.0858213, lng: -61.8596279};
-  var cubaLatLng = {lat: 21.4653347, lng: -84.0583324};
   var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 4,
     center: {
@@ -12,70 +13,87 @@ function initMap(easyMap) {
     },
   });
 
-  const markerCorrect = new google.maps.Marker({
+  var markerBer = new google.maps.Marker({
     position: bermudaLatLng,
     map: map,
     title: "Bermuda",
     label: {
-        fontSize: "25pt",
-        text: "Bermuda"
-    }
-})
+      fontSize: "25pt",
+      text: "Bermuda",
+    },
+  });
 
-    var markerIncorrect = new google.maps.Marker({
+  var markerJam = new google.maps.Marker({
     position: jamaicaLatLng,
     map: map,
     title: "Jamaica",
     label: {
-        fontSize: "25pt",
-        text: "Jamaica"
-    }
-    })
+      fontSize: "25pt",
+      text: "Jamaica",
+    },
+  });
 
-        var markerIncorrect = new google.maps.Marker({
+  var markerBar = new google.maps.Marker({
     position: barbadosLatLng,
     map: map,
     title: "Barbados",
     label: {
-        fontSize: "25pt",
-        text: "Barbados"
-    }
-    })
+      fontSize: "25pt",
+      text: "Barbados",
+    },
+  });
 
-        var markerIncorrect = new google.maps.Marker({
+  var markerAnt = new google.maps.Marker({
     position: antiguaLatLng,
     map: map,
     title: "Antigua",
     label: {
-        fontSize: "25pt",
-        text: "Antigua"
-    }
-    })
+      fontSize: "25pt",
+      text: "Antigua",
+    },
+  });
 
-        var markerIncorrect = new google.maps.Marker({
+  var markerCub = new google.maps.Marker({
     position: cubaLatLng,
     map: map,
     title: "Cuba",
     label: {
-        fontSize: "25pt",
-        text: "Cuba"
-    }
-    })
+      fontSize: "25pt",
+      text: "Cuba",
+    },
+  });
 
+  markerBer.addListener("click", () => {
+    $("#map-page").addClass("hide");
+    $("#correct-answers").addClass("hide");
+    $("#final-page").removeClass("hide");
+    $("#yesCaptain").removeClass("hide");
+  });
 
-  
-  markerCorrect.addListener("click", () => {
-      $("#map-page").addClass("hide");
-       $("#correct-answers").addClass("hide");
-        $("#final-page").removeClass("hide");
-        $("#yesCaptain").removeClass("hide");
-  })
+  markerJam.addListener("click", () => {
+    $("#map-page").addClass("hide");
+    $("#correct-answers").addClass("hide");
+    $("#final-page").removeClass("hide");
+    $("#noCaptain").removeClass("hide");
+  });
+  markerBar.addListener("click", () => {
+    $("#map-page").addClass("hide");
+    $("#correct-answers").addClass("hide");
+    $("#final-page").removeClass("hide");
+    $("#noCaptain").removeClass("hide");
+  });
 
-  markerIncorrect.addListener("click", () => {
-      $("#map-page").addClass("hide");
-       $("#correct-answers").addClass("hide");
-        $("#final-page").removeClass("hide");
-        $("#noCaptain").removeClass("hide");
-  })
+  markerAnt.addListener("click", () => {
+    $("#map-page").addClass("hide");
+    $("#correct-answers").addClass("hide");
+    $("#final-page").removeClass("hide");
+    $("#noCaptain").removeClass("hide");
+  });
 
+  markerCub.addListener("click", () => {
+    $("#map-page").addClass("hide");
+    $("#correct-answers").addClass("hide");
+    $("#final-page").removeClass("hide");
+    $("#noCaptain").removeClass("hide");
+  });
 }
