831e8da19cf1eb460b109af8ba2ee6070012b210
stefbez <stefbez@hotmail.com>
2020-09-08 19:13:37 +0000

    Add questions to the questions page

---
+++ assets/js/questions.js
@@ -0,0 +1,215 @@
+// Jamaica - Easy
+
+const questionEasy = [
+  {
+    question:
+      "Who holds the men's world record for the 100m sprint at 9.58 seconds?",
+    easyA: "Usain Bolt",
+    easyB: "Tyson Gay",
+    easyC: "Adam Gemili",
+    correct: "easyA",
+  },
+  {
+    question:
+      "Which singer sung ‘No women, no cry’ which was released in 1979?",
+    easyA: "Kurt Cobain",
+    easyB: "Bob Marley",
+    easyC: "Michael Jackson",
+    correct: "easyB",
+  },
+  {
+    question:
+      "Which 1993 film is this quote from: 'Feel the rythm! Feel the rhyme! Get on up, it's bobsled time!",
+    easyA: "Forrest Gump",
+    easyB: "Toy Story",
+    easyC: "Cool Runnings",
+    correct: "easyC",
+  },
+  {
+    question:
+      "Who is the secret service agent in the films ‘Spectre’ and ‘Casino Royale?’",
+    easyA: "James Bond",
+    easyB: "John Mason",
+    easyC: "Mallory Kane",
+    correct: "easyA",
+  },
+  {
+    question: "Which of these is a car produced by Austin/Rover?",
+    easyA: "Montego",
+    easyB: "Astra",
+    easyC: "Mondeo",
+    correct: "easyA",
+  },
+  {
+    question: "Which of these types of music uses steel drums?",
+    easyA: "Rock",
+    easyB: "Reggae",
+    easyC: "Pop",
+    correct: "easyC",
+  },
+];
+
+// Barbados - Medium
+
+const questionMed = [
+  {
+    question: "Who sung ‘Umbrella’ in 2007 and ‘We Found Love?’ In 2011?",
+    medA: "Rihanna",
+    medB: "Ellie Goulding",
+    medC: "Beyonce",
+    correct: "medA",
+  },
+  {
+    question: "Which aircraft reached speeds on 2,179 km/h?",
+    medA: "Airbus A320",
+    medB: "Concord",
+    medC: "Airbus Beluga",
+    correct: "medB",
+  },
+  {
+    question: "Which rum has the world’s oldest commercial distillery?",
+    medA: "Captain Morgan",
+    medB: "Kraken",
+    medC: "Mount Gay",
+    correct: "medC",
+  },
+  {
+    question: "Which sport was believed to be first played in 1646?",
+    medA: "Baseball",
+    medB: "Cricket",
+    medC: "Tennis",
+    correct: "medB",
+  },
+  {
+    question: "Where in London is the Worlds Smallest Police Station?",
+    medA: "Trafalgar Square",
+    medB: "Wembley Stadium",
+    medC: "Camden Town",
+    correct: "medA",
+  },
+  {
+    question: "What are Silver Spoon known for making?",
+    medA: "Sugar",
+    medB: "Coffee",
+    medC: "Tea",
+    correct: "medA",
+  },
+  {
+    question: "What are these types of? Angling, bait and fly",
+    medA: "Swimming",
+    medB: "Curling",
+    medC: "Fishing",
+    correct: "medC", 
+  },
+  {
+    question: "Which two colours mix together to create green?",
+    medA: "Blue and Yellow",
+    medB: "Yellow and Red",
+    medC: "Red and Blue",
+    correct: "medA",
+  },
+  {
+    question: "What sport famously takes place in the town of Ascot?",
+    medA: "Archery",
+    medB: "Horse Racing",
+    medC: "Rugby",
+    correct: "medB",
+  },
+  {
+    question: "Which book contains the Old and New testaments?",
+    medA: "The Bible",
+    medB: "Harry Potter and the Philosopher's Stone",
+    medC: "Bridge Jones Diary",
+    correct: "medA",
+  },
+];
+
+// Bermuda - Hard
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const questionHard = [
+  {
+    question: "Which musical by Lin-Manuel Miranda is mostly sung and rapped throughout?",
+    hardA: "Hamilton",
+    hardB: "Wicked",
+    hardC: "Dear Evan Hansen",
+    correct: "hardA",
+  },
+  {
+    question: "Which ocean separates North America and the United Kingdom?",
+    hardA: "Atlantic Ocean",
+    hardB: "Pacific Ocean",
+    hardC: "Indian Ocean",
+    correct: "hardA",
+  },
+  {
+    question: "Which rum is the trademark rum to use in a Dark 'N' Stormy Cocktail?",
+    hardA: "Kraken",
+    hardB: "Havana Club",
+    hardC: "Goslings",
+    correct: "hardC",
+  },
+  {
+    question: "Which member of The Beatles was murdered?",
+    hardA: "George Harrison",
+    hardB: "John Lennon",
+    hardC: "Paul McCartney",
+    correct: "hardB",
+  },
+  {
+    question: "Which vegetable is related to shallots and can be made into rings or bhajis?",
+    hardA: "Tomato",
+    hardB: "Courgette",
+    hardC: "Onion",
+    correct: "hardC", 
+  },
+  {
+    question: "What TV show shown on Channel 4 strands contestants on an island to see how they survive?",
+    hardA: "Castaway",
+    hardB: "Shipwrecked",
+    hardC: "The Hotel",
+    correct: "hardB",
+  },
+  {
+    question: "Which flag represents the United Kingdom?",
+    hardA: "Union Jack",
+    hardB: "Royal Standard",
+    hardC: "Jolly Rodger",
+    correct: "hardA",
+  },
+  {
+    question: "Which football team has scored the fastest goal in Premier League history?",
+    hardA: "Crystal Palace",
+    hardB: "Manchester City",
+    hardC: "Southampton",
+    correct: "hardC",
+  },
+  {
+    question: "Which of these would you wear in hot weather?",
+    hardA: "Chinos",
+    hardB: "Jeans",
+    hardC: "Shorts",
+    correct: "hardC",
+  },
+  {
+    question: "What shape can be an isosceles, equilateral, scalene or a right-angle?",
+    hardA: "Square",
+    hardB: "Triangle",
+    hardC: "Circle",
+    correct: "hardB",
+  },
+];
